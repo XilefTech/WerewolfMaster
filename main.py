ec67b615379100c9b72ca1afe0814ba484f1d2dc
XilefTech <52332196+XilefTech@users.noreply.github.com>
2023-06-24 11:43:05 +0200

    probably a relatively solid basic name-picking system

--- main.py
+++ main.py
@@ -1,22 +1,45 @@
 from html import escape
-from flask import Flask, redirect, render_template, request
+from flask import Flask, make_response, redirect, render_template, request
 
 app = Flask(__name__)
+players = []
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         name = request.form["name"].lower()
-        return redirect(f"/user/{name}")
+        if name not in players:
+            players.append(name)
+            
+            response = make_response(redirect(f"/user"))
+            response.set_cookie("username", name)
+            return response
+        else:
+            return render_template("index.html", error="Name already taken")
     
+    name = request.cookies.get("username")
+    players.remove(name) if name in players else None
 
-    return render_template("index.html")
+    response = make_response(render_template("index.html"))
+    response.set_cookie("username", "", expires=0)
+    return response
 
-@app.route("/user/<username>")
-def userpage(username):
-    username = username.capitalize()
-    return render_template("user.html", username=escape(username))
+@app.route("/user")
+def userpage():
+    username = request.cookies.get("username")
+    if username:
+        return render_template("user.html", username=escape(username.capitalize()))
+    else:
+        return redirect("/")
 
+@app.route("/logout")
+def logout():
+    name = request.cookies.get("username")
+    players.remove(name)
+
+    response = make_response(redirect("/"))
+    response.set_cookie("username", "", expires=0)
+    return response
 
 @app.route("/narrator")
 def narratorMain():
