75f9aea9ea2a2ec677c8d69b5e401bb7a0f4c971
XilefTech <52332196+XilefTech@users.noreply.github.com>
2023-06-24 11:55:09 +0200

    split up index(), changed behavior for logged in users

--- main.py
+++ main.py
@@ -4,25 +4,29 @@ from flask import Flask, make_response, redirect, render_template, request
 app = Flask(__name__)
 players = []
 
-@app.route("/", methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        name = request.form["name"].lower()
-        if name not in players:
-            players.append(name)
-            
-            response = make_response(redirect(f"/user"))
-            response.set_cookie("username", name)
-            return response
-        else:
-            return render_template("index.html", error="Name already taken")
-    
-    name = request.cookies.get("username")
-    players.remove(name) if name in players else None
 
-    response = make_response(render_template("index.html"))
-    response.set_cookie("username", "", expires=0)
-    return response
+@app.get("/")
+def index_get():
+    # If the user is already logged in, redirect to the user page
+    if request.cookies.get("username"):
+        return redirect("/user")
+    else:
+        return render_template("index.html")
+
+
+@app.post("/")
+def index_post():
+    # Check entered name and redirect to user page if it is not taken
+    name = request.form["name"].lower()
+    if name not in players:
+        players.append(name)
+        
+        response = make_response(redirect(f"/user"))
+        response.set_cookie("username", name)
+        return response
+    else:
+        return render_template("index.html", error="Name already taken")
+
 
 @app.route("/user")
 def userpage():
@@ -31,6 +35,7 @@ def userpage():
         return render_template("user.html", username=escape(username.capitalize()))
     else:
         return redirect("/")
+
 
 @app.route("/logout")
 def logout():
@@ -41,9 +46,11 @@ def logout():
     response.set_cookie("username", "", expires=0)
     return response
 
+
 @app.route("/narrator")
 def narratorMain():
     return render_template("narrator.html")
+
 
 while True:
     app.run(host='192.168.178.58', port=80)
