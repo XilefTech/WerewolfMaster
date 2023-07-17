3ffdbf9726e2f8ab3b9ee04b593ea4d371480afe
XilefTech <52332196+XilefTech@users.noreply.github.com>
2023-07-18 00:21:17 +0200

    added playerstats

--- gameData.py
+++ gameData.py
@@ -3,5 +3,6 @@ playerTimeout = {}
 timeout = 10 # in seconds
 roles = {'wolf': 2, 'witch': 1, 'seer': 1, 'slut': 1, 'cupid': 1, 'thief': 1, 'winking_girl': 1} # cupid = amor
 assignedRoles = {}
+playerStats = {}
 gamestate = 0
 gamestates = ["pre-round", "running"]
